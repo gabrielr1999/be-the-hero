7c97b14c7d54478f4ad63eb23f28dc264e006fb4
Gabriel Renan <gabrielrenansilva@hotmail.com>
2020-03-29 19:30:50 -0300

    Mobile da aplicação

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -67,7 +67,7 @@ export default function Detail() {
                 <Text style={style.heroDescription}>Entre em contato:</Text>
 
                 <View style={style.actions}>
-                    <TouchableOpacity style={style.action} conPress={sendWhatsapp()}>
+                    <TouchableOpacity style={style.action} conPress={sendWhatsapp}>
                         <Text style={style.actionText}>WhatsApp</Text>
                     </TouchableOpacity>
 
